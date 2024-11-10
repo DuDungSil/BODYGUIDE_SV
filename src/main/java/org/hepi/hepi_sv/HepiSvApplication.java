2d1c2605ab8f2281e0794f09691a52937358ca00
DuDungSil <121501087+DuDungSil@users.noreply.github.com>
2024-11-11 06:51:18 +0900

    init

---
+++ src/main/java/org/hepi/hepi_sv/HepiSvApplication.java
@@ -0,0 +1,18 @@
+package org.hepi.hepi_sv;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HepiSvApplication {
+    private static final Logger logger = LoggerFactory.getLogger(HepiSvApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(HepiSvApplication.class, args);
+        logger.info("==================== START HEPI SERVER ====================");
+    }
+
+}
