eae702fa63b2fe90f853925b473e5fcc4d79faa7
DuDungSil <121501087+DuDungSil@users.noreply.github.com>
2025-01-17 19:54:50 +0900

    commit

--- src/main/java/org/bodyguide_sv/auth/jwt/TokenProvider.java
+++ src/main/java/org/bodyguide_sv/auth/jwt/TokenProvider.java
@@ -42,7 +42,7 @@ public class TokenProvider {
 
     private SecretKey secretKey;
 
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 1L; // 1분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30L; // 30분
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60L * 24 * 7; // 7일
     private static final String KEY_ROLE = "role";
 
